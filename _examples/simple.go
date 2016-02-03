ac5a09aa9c214897c4b0cdb2685da53671984358
Matt Butcher <mbutcher@engineyard.com>
2016-02-03 07:05:34 -0700

    Added extra spaces in example.

--- _examples/simple.go
+++ _examples/simple.go
@@ -9,10 +9,13 @@ import (
 func main() {
 	p := progress.NewIndicator()
 	p.Start("Starting")
+
 	time.Sleep(2 * time.Second)
 	p.Message("Still going")
+
 	time.Sleep(2 * time.Second)
 	p.Done("Done")
+
 	time.Sleep(20 * time.Millisecond)
 	println("The end")
 }
